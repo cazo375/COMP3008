3a5b864796f9c6fdb643137e6562fc6676fc752b
NJWewala <njc.wewala@gmail.com>
2017-04-04 22:50:03 -0400

    fileoutput stream

--- app/src/main/java/com/opticnerve/nccc/MainActivity.java
+++ app/src/main/java/com/opticnerve/nccc/MainActivity.java
@@ -231,6 +231,10 @@ public class MainActivity extends Activity {
                         }
                         String string = old_pass_out[0] + "," + old_pass_out[1] + "," + old_pass_out[2] + "," + old_pass_out[3] + ","
                                 + old_pass_out[4] + "," + old_pass_out[5] + "," + old_pass_out[6] + "," + old_pass_out[7] + "\n";
+//                        FileOutputStream outputStream;
+//                        outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+//                        outputStream.write(string.getBytes());
+//                        outputStream.close();
                         //(niresh) changes
                         Boolean flag = isExternalStorageWritable();
                         Boolean flag2 = isExternalStorageReadable();
